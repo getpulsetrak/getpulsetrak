79b893b748ebe82f0677e3e18be3a829830305db
Gregory Mihalik <gregory.mihalik@gmail.com>
2020-05-06 18:23:15 -0400

    Updated button link

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $("#connectStrava").click(function() {
   console.log("Go connect to Strava");
-  //window.location = "https://www.strava.com/oauth/authorize?client_id=30652&response_type=code&redirect_uri=https://agile-mountain-82084.herokuapp.com/connected&approval_prompt=force&scope=read,activity:write";
-  window.location = "https://www.strava.com/oauth/authorize?client_id=30652&response_type=code&redirect_uri=http://localhost:5000/pulsetrak&approval_prompt=force&scope=read,activity:write";
+  window.location = "https://www.strava.com/oauth/authorize?client_id=30652&response_type=code&redirect_uri=https://getpulsetrak.com/&approval_prompt=force&scope=read,activity:write";
+  //window.location = "https://www.strava.com/oauth/authorize?client_id=30652&response_type=code&redirect_uri=http://localhost:5000/pulsetrak&approval_prompt=force&scope=read,activity:write";
 });
